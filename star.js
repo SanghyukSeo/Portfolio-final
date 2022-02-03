a4d96167fb49c896a4599e777395603323220e90
SanghyukSeo <97425414+SanghyukSeo@users.noreply.github.com>
2022-02-03 01:06:30 -0500

    2.3.2022
    layout

---
+++ star.js
@@ -0,0 +1,35 @@
+
+
+function setup() {
+    let cnv = createCanvas($("#star").outerWidth(), $("#star").outerHeight());
+    cnv.parent('star');
+  }
+  
+function windowResized() {
+    resizeCanvas($("#star").outerWidth(), $("#star").outerHeight());
+  }
+  function draw() {
+    background(255);
+    
+    push();
+    star(mouseX, mouseY,  Math.floor(map(mouseX,0,width,30,70)), Math.floor(map(mouseY,0,height,30,70)), 40);
+    pop();
+  
+  }
+  
+  
+  function star(x, y, radius1, radius2, npoints) {
+    
+    let angle = TWO_PI / npoints;
+    let halfAngle = angle / 2.0;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius2;
+      let sy = y + sin(a) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a + halfAngle) * radius1;
+      sy = y + sin(a + halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+}
